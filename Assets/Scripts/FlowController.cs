9bcd6c62678a13ea80eace29356b1648a00c345d
Daisuke Hosomy <jackson.daniel.17dh@gmail.com>
2022-07-25 22:01:09 -0400

    Criado controle de fluxo

---
+++ Assets/Scripts/FlowController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowController : MonoBehaviour
+{
+    public void OpenMenu()
+    {
+        Debug.Log("ABRIU MENU");
+    }
+
+    public void OpenGame()
+    {
+        Debug.Log("ABRIU JOGO");
+    }
+
+    public void OpenResults()
+    {
+        Debug.Log("ABRIU RESULTADOS");
+    }
+}
