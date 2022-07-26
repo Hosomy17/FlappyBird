c627977ba4320df0cf4922655b6b129d7b07297a
Daisuke Hosomy <jackson.daniel.17dh@gmail.com>
2022-07-25 20:50:41 -0400

    Parallax Implementado

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer _meshBackground;
+
+    [SerializeField]
+    private float _speedBackground;
+    
+    [SerializeField]
+    private MeshRenderer _meshGround;
+
+    [SerializeField]
+    private float _speedGround;
+
+    public void StartParallax()
+    {
+        StartCoroutine(UpdateParallax());
+    }
+    
+    public void StopParallax()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator UpdateParallax()
+    {
+        while (true)
+        {
+            yield return null;
+            _meshBackground.material.mainTextureOffset += new Vector2(_speedBackground * Time.deltaTime, 0);
+            _meshGround.material.mainTextureOffset += new Vector2(_speedGround * Time.deltaTime, 0);
+        }
+    }
+}
