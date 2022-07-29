da73aae848ee7aa6572c2a1764bf8b45b76913f5
marcosduarte94 <mvcd.pje22@uea.edu.br>
2022-07-28 23:42:03 -0400

    CORREÇÃO DE BUG
    Bug corrigido no botão de retry

--- Assets/Scripts/OpenGameButton.cs
+++ Assets/Scripts/OpenGameButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Menu : MonoBehaviour
+public class OpenGameButton : MonoBehaviour
 {
     [SerializeField]
     private AudioClip _audioSource;
